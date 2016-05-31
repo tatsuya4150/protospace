889631880760bb5e9c8c0c75f59e82cb48087640
TATSUYA <tatsuya415077@gmail.com>
2016-05-31 12:20:53 +0900

    packaged sign_up function

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:name, :avatar, :email, :password, :member, :profile, :works]
+
 end
