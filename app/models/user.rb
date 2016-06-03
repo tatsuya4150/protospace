9fb467a67f90500abde10e65655820642c108575
TATSUYA <tatsuya415077@gmail.com>
2016-06-03 13:18:13 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :authentication_keys => [:name, :avatar, :email, :password, :member, :profile, :works]
+         :authentication_keys => [:email]
+
+validates :name, presence: true
 
 end
