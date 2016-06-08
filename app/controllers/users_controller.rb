e0f6adcfa69393f9cee0478a7bd7faf148992795
TATSUYA <tatsuya415077@gmail.com>
2016-06-08 22:17:45 +0900

    modify view_files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
     @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :avatar, :member, :profile, :works)
   end
 end
