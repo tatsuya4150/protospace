b7515ceafcd73e5553499b4cd0fd61809ddc4908
TATSUYA <tatsuya415077@gmail.com>
2016-06-17 23:33:01 +0900

    edit routes.rb for create action

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
   get  '/prototypes/show'   =>    'prototypes#show'
+  post '/prototypes'        =>    'prototypes#create'
 end
