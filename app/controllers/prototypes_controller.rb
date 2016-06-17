69df415b1bcf9d6ae19e1b13888b7d24eedba512
TATSUYA <tatsuya415077@gmail.com>
2016-06-17 23:33:50 +0900

    add create action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -6,4 +6,24 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.new
     @prototype.images.build
   end
+
+  def create
+    binding.pry
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path, notice: '投稿完了しました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(
+      :title,
+      :catch_copy,
+      :concept,
+      images_attributes: [:id, :image, :role, :prototype_id]
+      )
+  end
 end
