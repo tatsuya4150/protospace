93c7fe5f60be6f40836aca49eb2b82e1b7490cb5
TATSUYA <tatsuya415077@gmail.com>
2016-06-18 18:57:10 +0900

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :users, only: [:show, :edit, :update]
-
-  get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
-  post '/prototypes'        =>    'prototypes#create'
+  resources :prototypes, only: [:index, :new, :show, :create]
 end
