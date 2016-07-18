a20ecb474830f22888b7050c0032833c0a57508e
TATSUYA <tatsuya415077@gmail.com>
2016-07-18 23:45:50 +0900

    add associations in prototype.rb

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,9 +1,12 @@
 class Prototype < ActiveRecord::Base
+#associations
   has_many :images
   belongs_to :user
+  has_many :likes
+
   accepts_nested_attributes_for :images, reject_if: proc { |attributes| attributes['image'].blank? }
 
-
+#validates
   validates :catch_copy, :concept, :title, presence: true
 end
 
