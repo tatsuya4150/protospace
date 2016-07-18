e0dbdffef4e690c4a3596a3b679caa895108346b
TATSUYA <tatsuya415077@gmail.com>
2016-07-19 03:57:28 +0900

    Add config not to generate test_file, helper_file, coffee_script_file

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module Protospace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+     config.generators do |g|
+      g.test_framework false
+      g.assets     false
+      g.helper     false
+    end
   end
 end
