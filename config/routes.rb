364494fa407eb10f08ebdee55744feb34b9e6f73
TATSUYA <tatsuya415077@gmail.com>
2016-07-20 01:00:04 +0900

    Add routing to comments_controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :prototypes, only: [:index, :new, :show, :create]  do
     scope module: :prototypes do
       resources :likes, only: [:create, :destroy]
+      resources :comments, only: [:create]
     end
   end
 end
